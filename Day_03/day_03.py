33c3e2a2f4af850b94ad383ab98b6fe2012e55ca
GAcs <Geza.Acs@heitec.hu>
2022-12-04 02:09:36 +0100

    day_03

---
+++ Day_03/day_03.py
@@ -0,0 +1,33 @@
+lines = []
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+UPPER=ord('a')-ord('A')+26
+def part_1():
+    sum=0
+    for line in lines:
+        size=len(line)
+        first=set(line[:size//2])
+        second=set(line[size//2:])
+        inter = first.intersection(second)
+        shared=list(inter)
+        diff=ord(shared[0])-ord('a')+1
+        diff += UPPER if diff < 1 else 0
+        sum+=diff
+    return sum
+
+def part_2():
+    sum=0
+    size=len(lines)
+    for ind in range(0,size,3):
+        first=set(lines[ind])
+        second=set(lines[ind+1])
+        third=set(lines[ind+2])
+        inter = first.intersection(second)
+        shared=list(inter.intersection(third))
+        diff=ord(shared[0])-ord('a')+1
+        diff += UPPER if diff < 1 else 0
+        sum+=diff
+    return sum
+
+print(part_2())
